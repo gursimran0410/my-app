fa2b20ee81d3871cff36d6cf9f059adfdfeb331a
Gursimran Singh <gursimransingh154@gmail.com>
2020-11-12 13:50:27 +0530

    updates

--- src/features/cards/card.js
+++ src/features/cards/card.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect} from 'react'
 import {connect} from 'react-redux'
 import CardsShow from './renderCards'
 import '../../App.css'
